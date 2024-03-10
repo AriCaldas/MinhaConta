1ae008b7ce9771743ca8b3f90706763f3f768851
Ari <arimauriciomonteiro@gmail.com>
2024-03-10 17:23:09 -0300

    Fix bug dark Mode, primeira parte

--- script.js
+++ script.js
@@ -6,14 +6,32 @@ let contasContainer = document.querySelector(".contasContainer");
 
 function darkMode(){
     let body = document.body;
+    let contasLight = document.querySelector(".contasLight");
+    let contasDark = document.querySelector(".contasDark");
+    let darkBtn = document.querySelector(".darkMode");
 
     if(dark == false){
+        darkBtn.style.backgroundColor = "#fff";
+        darkBtn.style.color = "#000";
         body.style.backgroundColor = "#000";
         body.style.color = "#fff";
+        if(contasLight){
+            console.log(contasLight);
+            contasLight.classList.remove("contasLight");
+            contasLight.classList.add("contasDark");
+        }
         dark = true;
+        console.log(dark);
     }else{
+        darkBtn.style.backgroundColor = "#000";
+        darkBtn.style.color = "#fff";
         body.style.backgroundColor = "#fff";
         body.style.color = "#000";
+        if(contasDark){
+            console.log("contasDark");
+            contasDark.classList.remove("contasDark");
+            contasDark.classList.add("contasLight");
+        }
         dark = false;
     }
 }
@@ -43,11 +61,19 @@ function adicionar(e){
         alert("Preencha todos os campos.");
         nomeConta.focus();
     }else{
-        novaConta.classList.add("contas");
-        novaConta.innerHTML = `<p>${nomeConta.value}</p>`;
-        contasContainer.appendChild(novaConta);
-        fieldReset();
-        cancelar();
+        if(dark == false){
+            novaConta.classList.add("contasLight");
+            novaConta.innerHTML = `<p>${nomeConta.value}</p>`;
+            contasContainer.appendChild(novaConta);
+            fieldReset();
+            cancelar();
+        }else{
+            novaConta.classList.add("contasDark");
+            novaConta.innerHTML = `<p>${nomeConta.value}</p>`;
+            contasContainer.appendChild(novaConta);
+            fieldReset();
+            cancelar();
+        }
     }
 }
 
@@ -58,12 +84,12 @@ function fieldReset(){
      contasContainer.value = "";
 }
 
-function mostrarDados(){
+// function mostrarDados(){
     
-}
+// }
 
 document.querySelector(".botaoMais").addEventListener("click", chamarModal);
 document.querySelector(".btnCancelar").addEventListener("click", cancelar);
 document.querySelector(".btnAdicionar").addEventListener("click", adicionar);
 document.querySelector(".darkMode").addEventListener("click", darkMode);
-document.querySelector(".contas").addEventListener("click", mostrardados);
+// document.querySelector(".contas").addEventListener("click", mostrardados);
